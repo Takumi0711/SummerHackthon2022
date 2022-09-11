c2bf1e4a0cfe3731d2bdb4943a8db011ccba76f9
yoshiki <tanayoshi78907890@icloud.com>
2022-09-11 16:22:35 +0900

    set func score

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ScoreScript : MonoBehaviour
 {
 
-    private static Canvas scoreCanvas;
+    // private static Canvas scoreCanvas;
 
-    private void Awake(){
-        scoreCanvas = GetComponent<Canvas>();
-    }
+    // private void Awake(){
+    //     scoreCanvas = GetComponent<Canvas>();
+    // }
 
     public static void ShowScorePanel(){
         Time.timeScale = 0f; //ゲーム内時間の停止
-        scoreCanvas.enabled = true;//スコアのパネルを表示する
+        double Score = Math.Round(Time.time, 1, MidpointRounding.AwayFromZero);
+        // Type == Number の場合
+        naichilab.RankingLoader.Instance.SendScoreAndShowRanking (Score);
     }
 
 }
